cf5ae54d2a9dc97dfa0d3ac38c2ae56f68851505
free2610 <164719687+free2610@users.noreply.github.com>
2026-07-27 23:32:11 +0500

    Update urls.py

--- basic/urls.py
+++ basic/urls.py
@@ -30,3 +30,5 @@ urlpatterns = [
          auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), 
          name='password_reset_complete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
