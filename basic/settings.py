dafdeea836dcd44db868fa587cedf6967cda971e
free2610 <164719687+free2610@users.noreply.github.com>
2026-07-27 23:32:36 +0500

    Update settings.py

--- basic/settings.py
+++ basic/settings.py
@@ -141,4 +141,7 @@ STATICFILES_STORAGE = (
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
