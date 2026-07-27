6dd759cbdf06e9deeddd7fdd1eb2597d8764350a
free2610 <164719687+free2610@users.noreply.github.com>
2026-07-27 22:49:19 +0500

    Update settings.py

--- basic/settings.py
+++ basic/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # YANGI QATOR:
     'basic.middleware.LoginRequiredMiddleware', 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 ROOT_URLCONF = 'basic.urls'
 
@@ -129,7 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
